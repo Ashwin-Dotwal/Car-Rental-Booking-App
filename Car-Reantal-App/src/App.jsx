9213b56656092f368c8d33af915c1ebbede0a85f
Ashwin Dotwal <ashwindotwal09@gmail.com>
2025-09-06 17:53:04 +0530

    All Front-End part is complite of this project

--- Car-Reantal-App/src/App.jsx
+++ Car-Reantal-App/src/App.jsx
@@ -1,27 +1,44 @@
 import React, { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+
 import NavBar from "./components/NavBar";
-import { Route, Routes, useLocation } from "react-router-dom";
-import Home from "./pages/Home";
-import CarDetails from "./pages/CarDetails";
-import Cars from "./pages/Cars";
-import MyBooking from "./pages/MyBooking";
-import CarCard from "./components/CarCard";
 import Footer from "./components/Footer";
 
+import Home from "./pages/Home";
+import Cars from "./pages/Cars";
+import CarDetails from "./pages/CarDetails";
+import MyBooking from "./pages/MyBooking";
+import Dashboard from "./pages/owner/Dashboard";
+import Layout from "./pages/owner/Layout";
+import AddCar from "./pages/owner/AddCar";
+import ManageCars from "./pages/owner/ManageCars";
+import ManageBookings from "./pages/owner/ManageBookings";
+import Login from "./components/Login";
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
   const isOwnerPath = useLocation().pathname.startsWith("/owner");
+
   return (
     <>
+      {showLogin && <Login setShowLogin={setShowLogin} />}
       {!isOwnerPath && <NavBar setShowLogin={setShowLogin} />}
 
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
-        <Route path="/car-details/:id" element={<CarDetails />} />
         <Route path="/cars" element={<Cars />} />
+        <Route path="/car-details/:id" element={<CarDetails />} />
         <Route path="/my-bookings" element={<MyBooking />} />
+
+        {/* Owner Routes */}
+        <Route path="/owner" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="add-car" element={<AddCar />} />
+          <Route path="manage-cars" element={<ManageCars />} />
+          <Route path="manage-bookings" element={<ManageBookings />} />
+        </Route>
       </Routes>
-      <CarCard />
+
       {!isOwnerPath && <Footer />}
     </>
   );
